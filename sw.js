2d246328caf385c7049e75c4d834755e8251f969
sptecnologiaapps <sptecnologiaapps@gmail.com>
2026-09-13 11:35:36 -0300

    Atualiza cache da lista unificada

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='balanco-pro-v14-atualizado-shell-v5';
+const CACHE='balanco-pro-v14-atualizado-shell-v6';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./zxing.min.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
